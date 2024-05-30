75850aaeaa3a96f638cb95eac0317619fa85c223
eva <evamartinas289@gmail.com>
2024-05-30 10:57:37 +0700

    ground, cloud, and gamescene

---
+++ Characters/Cloud.swift
@@ -0,0 +1,8 @@
+//
+//  Cloud.swift
+//  GameJam
+//
+//  Created by Eva Martina on 30/05/24.
+//
+
+import Foundation
---
+++ Characters/Ground.swift
@@ -0,0 +1,8 @@
+//
+//  Ground.swift
+//  GameJam
+//
+//  Created by Eva Martina on 30/05/24.
+//
+
+import Foundation
